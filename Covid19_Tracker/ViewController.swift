602e4ad7021e0abb0266c452873d269bc9d4a9f6
Aravinda Rathnamalala <aravinda.rathnamalala@gmail.com>
2020-05-05 21:47:40 +0530

    initial commit  -> empty project added

---
+++ Covid19_Tracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Covid19_Tracker
+//
+//  Created by Harshana Rathnamalala on 5/5/20.
+//  Copyright © 2020 Harshana Rathnamalala. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
