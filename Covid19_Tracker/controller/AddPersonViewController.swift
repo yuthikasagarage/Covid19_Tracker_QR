abbb01cea919dfa773307779eba0ac6039b627cd
Yuthika <sandamalsagarage@gmail.com>
2020-06-03 03:07:10 +0530

    location added

--- Covid19_Tracker/controller/AddPersonViewController.swift
+++ Covid19_Tracker/controller/AddPersonViewController.swift
@@ -7,13 +7,51 @@
 //
 
 import UIKit
-
-class AddPersonViewController: UIViewController {
-
+import CoreLocation
+class AddPersonViewController: UIViewController , CLLocationManagerDelegate{
+   
+    let locationManager = CLLocationManager()
+    @IBOutlet weak var lblocation: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.addGestureRecognizer(UITapGestureRecognizer(target: self.view, action: #selector(UIView.endEditing(_:))))
     }
     
 
+    
+    @IBAction func location_tapped(_ sender: Any) {
+        
+         getCurrentLocation()
+        
+    }
+    
+    
+    func getCurrentLocation() {
+        // Ask for Authorisation from the User.
+        self.locationManager.requestAlwaysAuthorization()
+        self.locationManager.requestWhenInUseAuthorization()
+
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.startUpdatingLocation()
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+          
+             guard let locValue: CLLocationCoordinate2D = manager.location?.coordinate else {
+              
+              return
+              
+          }
+            
+          lblocation.text = "latitude = \(locValue.latitude), longitude = \(locValue.longitude)"
+      }
+    
 }
+    
+        
+  
+
